eead41783752fda85bee6a57e1e06e328915d3bb
disgrace504 <disgrace504@mail.ru>
2025-05-21 18:51:28 +0300

    Настроил modules scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,18 +1,34 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import webpack from 'webpack'
-import { BuildOptions } from './types/config'
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-  }
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  }
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+                    },
+                }
+            },
+            'sass-loader',
+        ]
+    }
 
-  return [typescriptLoader, cssLoader]
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './Counter.scss'
+import style from './Counter.module.scss'
 
 export function Counter() {
   const [count, setCount] = useState(0)
@@ -7,9 +7,8 @@ export function Counter() {
   const increment = () => {
     setCount(count + 1)
   }
-
   return (
-    <div>
+    <div className={style.btn}>
       <h2>{count}</h2>
       <button onClick={increment}>increment</button>
     </div>
