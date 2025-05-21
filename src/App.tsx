6a8d62d95b049f3eb9d0caac4d7686cd2cf2e822
disgrace504 <disgrace504@mail.ru>
2025-05-21 19:06:46 +0300

    Прикрутил react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import { Counter } from './components/Counter'
 import './index.scss'
+import MainPage from './pages/MainPage/MainPage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
-      a13213
-      <Counter />
+      <Link to={'/'}>Home</Link>
+      <Link to={'/about'}>About</Link>
+      <Routes>
+        <Route path={'/'} element={<MainPage />}></Route>
+        <Route path={'/about'} element={<AboutPage />}></Route>
+      </Routes>
     </div>
   )
 }
